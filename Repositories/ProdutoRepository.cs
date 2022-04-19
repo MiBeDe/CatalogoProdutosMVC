635506f70bbb410e7989fc6555eaea81a650eb96
Michel Belo <belodelf@gmail.com>
2022-04-19 13:48:59 -0300

    Caminho arquivo json fixo

--- Repositories/ProdutoRepository.cs
+++ Repositories/ProdutoRepository.cs
@@ -11,7 +11,8 @@ namespace CatalogoProdutosMVC.Repositories
     public class ProdutoRepository : IProdutoRepository
     {
         //private readonly CatalogoProdutosDbContext _context;
-        private string diretorio = "E:\\GitHubzin\\CatalogoProdutosMVC\\catalogoprodutoswebmvc-0d38f07c0ccb.json";
+        //private string diretorio = "E:\\GitHubzin\\CatalogoProdutosMVC\\catalogoprodutoswebmvc-0d38f07c0ccb.json";
+        private string diretorio = "Y:\\Github\\CatalogoProdutosProject\\CatalogoProdutos\\catalogoprodutoswebmvc-0d38f07c0ccb.json";
         private string projetoId;
         FirestoreDb _firestoreDb;
 
